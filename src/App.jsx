edcf87f1e1d04f0ab001d7b74e6ce73a76d2a019
Your Name <79807@student.amnuaysilpa.ac.th>
2026-05-13 20:09:20 +0700

    <|channel>thought <channel|>refactor: implement MainLayout and add AiChat route to App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,94 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
+
 import { ThemeProvider } from './context/ThemeContext'
+
 import TopNavBar from './components/TopNavBar'
+
 import Home from './pages/Home'
+
 import Team from './pages/Team'
+
 import Engineering from './pages/Engineering'
+
 import Sponsorship from './pages/Sponsorship'
+
 import PdfViewer from './pages/PdfViewer'
+
 import Merch from './pages/Merch'
+
+import AiChat from './pages/AiChat'
+
 import Footer from './components/Footer'
 
-function App() {
-  return (
-    <ThemeProvider>
-      <Router>
-        <div className="min-h-screen relative theme-root">
-          <div className="noise-overlay" />
-          <TopNavBar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/engineering" element={<Engineering />} />
-              <Route path="/sponsorship" element={<Sponsorship />} />
-              <Route path="/sponsorship-prospectus" element={<PdfViewer />} />
-              <Route path="/merch" element={<Merch />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </ThemeProvider>
-  )
+
+
+function MainLayout() {
+
+    return (
+
+        <>
+
+            <TopNavBar />
+
+            <main><Outlet /></main>
+
+            <Footer />
+
+        </>
+
+    )
+
 }
+
+
+
+function App() {
+
+    return (
+
+        <ThemeProvider>
+
+            <Router>
+
+                <div className="min-h-screen relative theme-root">
+
+                    <div className="noise-overlay" />
+
+                    <Routes>
+
+                        <Route element={<MainLayout />}>
+
+                            <Route path="/" element={<Home />} />
+
+                            <Route path="/team" element={<Team />} />
+
+                            <Route path="/engineering" element={<Engineering />} />
+
+                            <Route path="/sponsorship" element={<Sponsorship />} />
+
+                            <Route path="/sponsorship-prospectus" element={<PdfViewer />} />
+
+                            <Route path="/merch" element={<Merch />} />
+
+                        </Route>
+
+                        {/* The /ai route is outside MainLayout so it doesn't show the Navbar/Footer */}
+
+                        <Route path="/ai" element={<AiChat />} />
+
+                    </Routes>
+
+                </div>
+
+            </Router>
+
+        </ThemeProvider>
+
+    )
+
+}
+
+
 
 export default App
